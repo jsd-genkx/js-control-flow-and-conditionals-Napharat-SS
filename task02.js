e0122838e4e7a9e55a11d02cf673bb1020ece381
Napharat-ss <napharat.sroisom@gmail.com>
2025-07-15 11:46:49 +0700

    task02 solution

--- task02.js
+++ task02.js
@@ -6,12 +6,14 @@
 // const userInput = "42";
 // const userInput = "hello";
 // const userInput = 10;
+// const userInput = null;
 const userInput = NaN;
+console.log(typeof NaN);
 // Try changing to "hello", 10, null, NaN
 
 // if (!isNaN(Number(userInput))) {
-if (typeof userInput === 'number') {
-  console.log("It's a number!");
+if (typeof userInput === "number" && !isNaN(userInput)) {
+  console.log("It is a number.");
 } else {
-  console.log("It's not a number.");
+  console.log("It is NOT a number.");
 }
